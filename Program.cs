c2e40c882eb1aba097ffabb8c76fc16ae4c3e4bf
Hoetann <theepicbro300@gmail.com>
2017-10-08 16:19:18 +1100

    Intial commit

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Code
+{
+    class Program
+    {
+        static void Main(string[] args)                                                                     // This is a method called "Main". It is called when the programstarts.
+        {
+        Start:
+           double num01;
+           double num02;
+           
+
+           Console.WriteLine ("MULTIPLICATION CALCULATOR");
+           Console.Write ("Type a number: ");
+           num01 = Convert.ToDouble (Console.ReadLine ());
+           Console.Write ("Type another number to be multiplied: ");
+           num02 = Convert.ToDouble (Console.ReadLine ());
+           Console.WriteLine (num01 + " multiplied by " + num02 + " equals " + num01 * num02);
+
+                                                                                                            // This loops user back to start of calculator.
+           Console.ReadKey ();
+           Console.WriteLine(); 
+           goto Start;
+        }
+    }
+}
